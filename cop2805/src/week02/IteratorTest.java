5fd13147d66dfc091bb631fcb988bc667e328ad6
Bryant Vail <BryantVail@gmail.com>
2019-01-23 17:09:23 -0500

    update from work

--- cop2805/src/week02/IteratorTest.java
+++ cop2805/src/week02/IteratorTest.java
@@ -3,54 +3,82 @@ package week02;
 import java.util.Iterator;
 import java.util.LinkedList;
 
+//timeInVideo: 
+
 public class IteratorTest {
 	
-	LinkedList<Integer> myList;
-	private int listSize;
-	private StopWatch timer;
+	LinkedList<Integer> localList;
+	private int 		listSize;
+	private StopWatch 	timer;
+	private long 		startTime;
+	private long 		stopTime;
 	
 	public IteratorTest(int listSize) {
 		setListSize(listSize);
-		myList = new LinkedList<>();
-		
-		for(int i = 0; i < listSize; i++) {
-			myList.add(i);
-		}		
-		
-		timer = new StopWatch();
-		
-		
+		this.stopWatch = new StopWatch();
+		initialize();
 		
 	}
 
 	private void setListSize(int listSize) {
 		// TODO Auto-generated method stub
-		
+		this.listSize = listSize;
 		
 	}
 
 	public void execute() {
-		Iterator iterator = this.myList.iterator();
+		Iterator<Integer> iterator = this.localList.iterator();
+		stopWatch.start();
+		while(iter.hasNext()){
+			Integer integer = iterator.next();
+		}
+		stopWatch.stop();
+		iteratorElapsedTime = stopWatch.getElapsedTimeInMilliseconds();
+
+		//start again
+		stopWatch.start();
+		for(int index = 0; index< localList.size(); index++){
+			Integer integer = localList.get(index);
+		}
+		stopWatch.stop();
+		this.getAtElapsedTime = stopWatch.getElapsedTimeInMilliseconds();
+
+	}
+
+	public long getStartTime(){
+		return startTime;
+	}
+
+	public long getStopTime(){
+		return stopTime;
 	}
 	
 	public long getElapsedTimeInMilliseconds(){
 		
-		
-		
+		return this.getAtElapsedTime;
 	}
 	
 	public long getIteratorElapsedTimeInMilliseconds() {
 		
 	}
 
+	public void start(){
+		startTime = System.currentTimeMillis();
+	}
+	public void stop(){
+		stopTime = System.currentTimeMillis();
+	}
+
 	public long getListSize() {
-		// TODO Auto-generated method stub
-		int tally = 0;
-//		for(int i = 0; i<myList.size(); i++) {
-//			tally += myList.get(i);
-//		}
-//	
-//		return this.myList.size();
-		return this.myList.size();
+				
+		return this.localList.size();
+	}
+
+	public void initialize(){
+		this.localList = new ArrayList<Integer>();
+		for(int i = 0; i < this.listSize; i++) {
+			localList.add(i);
+		}	
+		
 	}
 }
